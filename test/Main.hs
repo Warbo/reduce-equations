73a19135ddbd30d5705480b1827fe4991f1c5af0
Chris Warburton <chriswarbo@gmail.com>
2016-02-19 13:53:43 +0000

    Stubbed out boilerplate

---
+++ test/Main.hs
@@ -0,0 +1,8 @@
+module Main where
+
+import           Test.Tasty             (defaultMain, testGroup, localOption)
+import           Test.Tasty.QuickCheck
+
+main = defaultMain $ testGroup "All tests" [
+
+  ]
